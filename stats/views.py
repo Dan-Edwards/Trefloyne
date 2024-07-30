619f71aa889ae7550304d2e49b4e868c2c779e19
Dan-Edwards <daned.3112@gmail.com>
2024-07-30 15:21:00 +0100

    login page

--- stats/views.py
+++ stats/views.py
@@ -36,7 +36,7 @@ def login_user(request):
 def logout_user(request):
         logout(request)
         messages.success(request, ("You have been successfully logged out!"))
-        return redirect('index')
+        return redirect('stats/index')
 
 
 def user_register(request):
